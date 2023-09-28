e569d3c047dbed62ee8ef24b9450e1d2d8c8a2e6
Malik Al-Ajam <malika76@nycstudents.net>
2023-09-28 12:07:56 -0400

    2ND

--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class CircleRunner {
+public class CircleRunner {
+    public static void main(String[] args) {
+
+        Circle circle1 = new Circle(5);
+        circle1.printArea();
+
+    }
 }
